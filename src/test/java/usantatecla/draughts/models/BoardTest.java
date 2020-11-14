660669f4f0db022387bdc38f6d5a5d424abadad6
cristoflop <cristofer.lc.99@gmail.com>
2020-11-14 17:32:51 +0100

    added test of not null board

--- src/test/java/usantatecla/draughts/models/BoardTest.java
+++ src/test/java/usantatecla/draughts/models/BoardTest.java
@@ -1,4 +1,21 @@
 package usantatecla.draughts.models;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class BoardTest {
+
+    private Board board;
+
+    @Before
+    public void before() {
+        this.board = new Board();
+    }
+
+    @Test
+    public void getPieceTest() {
+        Assert.assertNotNull(this.board);
+    }
+
 }
