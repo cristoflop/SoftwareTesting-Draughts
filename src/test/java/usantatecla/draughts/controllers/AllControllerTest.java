1a93184eb43ceeff2a9df0f669ffc8526245b318
cristoflop <cristofer.lc.99@gmail.com>
2020-11-17 16:21:41 +0100

    StartController test completed

---
+++ src/test/java/usantatecla/draughts/controllers/AllControllerTest.java
@@ -0,0 +1,12 @@
+package usantatecla.draughts.controllers;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        StartControllerTest.class
+})
+public class AllControllerTest {
+
+}
--- src/test/java/usantatecla/draughts/controllers/StartControllerTest.java
+++ src/test/java/usantatecla/draughts/controllers/StartControllerTest.java
@@ -1,4 +1,31 @@
 package usantatecla.draughts.controllers;
 
+import org.junit.Before;
+import org.junit.Test;
+import usantatecla.draughts.models.Game;
+import usantatecla.draughts.models.State;
+import usantatecla.draughts.models.StateValue;
+
+import static org.junit.Assert.assertEquals;
+
 public class StartControllerTest {
+
+    private Game game;
+    private State state;
+    private StartController startController;
+
+    @Before
+    public void before() {
+        this.game = new Game();
+        this.state = new State();
+        this.startController = new StartController(this.game, this.state);
+    }
+
+    @Test
+    public void testGivenStartControllerWhenCallStartMethodThenTurnChangeToInGame() {
+        assertEquals(this.state.getValueState(), StateValue.INITIAL);
+        this.startController.start();
+        assertEquals(this.state.getValueState(), StateValue.IN_GAME);
+    }
+
 }
